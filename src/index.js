9c55d4a6d299d025dafe48c2ecc248edf2c76550
Harrison Kelly <harrison.kelly@plexxi.com>
2016-06-13 14:23:24 -0400

    Numeric Comparison is wrong
    Change the comparison to less than or equal to.

--- src/index.js
+++ src/index.js
@@ -365,7 +365,7 @@
         var length = thresholds.length;
 
         for (var i = 0; i < length; i++) {
-            if (value < thresholds[i]) {
+            if (value <= thresholds[i]) {
                 return i;
             }
         }
